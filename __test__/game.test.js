ca7b9935e5822a7925f62df2524fa627d3accacf
Hamse Hashi <40909751+phoenix090@users.noreply.github.com>
2025-11-14 16:56:07 +0100

    Update game.test.js
    Use async/await

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,12 +2,9 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 describe('App', () => {
-  it('Contains the compiled JavaScript', async (done) => {
-    fs.readFile('./public/main.js', 'utf8', (err, data) => {
-      expect(err).toBe(null)
-      expect(data).toMatchSnapshot()
-      done()
-    })
+  it('Contains the compiled JavaScript', async () => {
+    const data = await fs.promises.readFile('./public/main.js', 'utf8')
+    expect(data).toMatchSnapshot()
   })
 })
 
